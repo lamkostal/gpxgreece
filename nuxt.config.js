ba11c6e0fd231156b0ce94e16aaaca3c18466f64
lamkostal <lamkostal@gmail.com>
2022-06-09 13:39:40 +0300

    quantacast in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
      
     ],
     script:[
-     
+     {src:quantacast.js,type:'text/javascript',async:true},
       {src:"https://unpkg.com/leaflet@1.7.1/dist/leaflet.js",
    integrity:"sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==",
    crossorigin:""},
